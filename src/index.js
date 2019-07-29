a9a482b95e03cd4aeb420574cfe3f6afa99a4198
kai <b10223052@yuntech.org.tw>
2019-07-29 17:37:16 +0800

    init file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import data from './pocker.json'
-import './style.css'
-console.log(data)
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router,Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
+import redux from './redux'
+import './style.scss'
+import Layout from './layout'
+
+let store = createStore(
+    redux,
+    applyMiddleware(logger)
+)
+
+// ReactDOM.render((
+// <Provider store={store}>
+//     <Router>
+//         <Route path="/" component={Layout}></Route>
+//     </Router>
+// </Provider>
+// ), document.getElementById('App'))
+
+ReactDOM.render((
+<Router>
+    <Route path="/" component={Layout}></Route>
+</Router>
+), document.getElementById('App'))
--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,14 @@ module.exports = {
     },
     module:{
         rules: [{
-            test: /\.(css)$/,
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        }, {
+            test: /\.(css|scss)$/,
             use: ['style-loader', // creates style nodes from JS strings
               'css-loader', // translates CSS into CommonJS
+              'sass-loader' // compiles Sass to CSS, using Node Sass by default
             ]
         }, {
             test: /\.(jpe?g|png|gif|svg)$/,
