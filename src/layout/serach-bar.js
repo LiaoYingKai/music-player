59c9af161280d52d733e7382ab027bd44bbfff7a
kai <b10223052@yuntech.org.tw>
2019-07-30 17:00:52 +0800

    add search bar style

--- src/layout/serach-bar.js
+++ src/layout/serach-bar.js
@@ -1,16 +1,16 @@
 import React from 'react'
+import serachIcon from '../images/search.png'
 
 function SearchBar(){
     return (
         <div className="search-bar">
             <div className="search-bar__search-input">
-                放大鏡
+                <img src={serachIcon} alt=""/>
+                <input type="text"></input>
             </div>
             <div className="search-bar__user">
-                <div>
-
-                </div>
-                Liao Kai
+                <div></div>
+                <p>Liao Kai</p>
             </div>
         </div>
     )
